d1c3f78e115bb9397ebce7b35a5ab7ddd2918a8f
etking19 <etking0072004@hotmail.com>
2014-10-16 18:38:55 +0800

    - Added window service description

--- WindowsMain/WindowsService1/ProjectInstaller.Designer.cs
+++ WindowsMain/WindowsService1/ProjectInstaller.Designer.cs
@@ -39,7 +39,9 @@
             // 
             // serviceInstaller1
             // 
-            this.serviceInstaller1.ServiceName = "Service1";
+            this.serviceInstaller1.Description = "Auto start Vistrol WCF connection";
+            this.serviceInstaller1.DisplayName = "Vistrol Window Service";
+            this.serviceInstaller1.ServiceName = "Vistrol Window Service";
             this.serviceInstaller1.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
             // 
             // ProjectInstaller
