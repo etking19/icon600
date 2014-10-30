19dbc9d1b7106e16dd397e3447da0c413d8035b8
etking19 <etking0072004@hotmail.com>
2014-10-30 11:56:05 +0800

    - add special checking for trial version

--- WindowsMain/WindowsFormServer/Program.cs
+++ WindowsMain/WindowsFormServer/Program.cs
@@ -100,6 +100,8 @@ namespace WindowsFormClient
             licenceChecker.StopCheck();
         }
 
+        private static DateTime expireDate = new DateTime(2014, 12, 1);
+
         static void licenceChecker_EvtLicenseCheckStatus(LicenseChecker.LicenseChecker checker, bool isValid)
         {
             try
@@ -112,6 +114,13 @@ namespace WindowsFormClient
             }
             catch (Exception)
             {
+            }
+
+            // TODO: check expire date
+            if (DateTime.Today.CompareTo(expireDate) > 0)
+            {
+                Application.Exit();
+                return;
             }
 
             if (!isValid)
